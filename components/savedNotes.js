0be01f948a63c44111ae81f8938914659f1db59c
Sidharth R P <sidharthrprakash@gmail.com>
2024-11-02 23:15:09 +0530

    Added supabase postgreSQL support

--- components/savedNotes.js
+++ components/savedNotes.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import { supabase } from '../lib/supabase'
 
-function SavedNotes() {
+export async function getServerSideProps() {
+  // Fetch data from your Supabase table
+  const { data, error } = await supabase.from('Notes').select();
+  console.log(data, error)
+  if (error) {
+    console.error('Error fetching data:', error);
+    return { props: { data: [] } };
+  }
+
+  return {
+    props: {
+      data: data || [],
+    },
+  };
+}
+
+function SavedNotes({ data }) {
+  getServerSideProps()
   return (
     <div className='mt-16'>
         <h1 className='text-xl font-bold'>Saved Notes</h1>
         <div className='mt-10 border-black border-2  h-48 w-32 text-center shadow-lg '>
             <h1 className='text-lg font-bold mt-5 uppercase'>Title</h1>
+            {Array.isArray(data) && data.length > 0 ? (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>{JSON.stringify(item)}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No data available.</p>
+      )}
             <p className='mt-5'>Content</p>
         </div>
     </div>
